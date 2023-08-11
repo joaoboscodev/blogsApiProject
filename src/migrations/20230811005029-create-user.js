4991af948249bf1b4bf68f36156edcd81119cf33
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-08-10 21:54:10 -0300

    feat: teste

---
+++ src/migrations/20230811005029-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async function(queryInterface, Sequelize) {
+    return await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primarKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING
+     },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+     },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+     }
+    }),
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
